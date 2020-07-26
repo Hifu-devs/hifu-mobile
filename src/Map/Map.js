98c656f9b6a57fe03d106ef3af6e8d78bca0081d
Megan Huggins <Muggins1265@gmail.com>
2020-07-26 14:05:32 -0600

    Add colored route between waypoints

--- src/Map/Map.js
+++ src/Map/Map.js
@@ -6,9 +6,9 @@ import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import { Marker, Callout } from 'react-native-maps';
 import * as Location from 'expo-location';
 import { AppLoading } from 'expo';
-import MapViewDirections from 'react-native-maps-directions';
-// import getEnvVars from '../../environment';
-// const { GOOGLE_API_KEY } = getEnvVars();
+// import MapViewDirections from 'react-native-maps-directions';
+import * as GOOGLE_API_KEY from '../../ENV';
+const apiKey = GOOGLE_API_KEY.default;
 
 // App Imports 
 import { setInitialLocation, setWayPoint } from './Api/actions';
@@ -26,7 +26,6 @@ class Map extends Component {
   }
 
   componentDidMount = async () => {
-    console.log('getEnvVars', getEnvVars);
 
     let { status } = await Location.requestPermissionsAsync();
     if (status !== 'granted') {
@@ -72,16 +71,6 @@ class Map extends Component {
       return <AppLoading />
     } else {
       const markers = this.makeMarkers();
-      console.log('markers', markers);
-
-    const origin = markers[0].props.coordinate;
-    
-    // if(markers.length >= 2) {
-    //   let destination = markers[1].props.coordinate;
-    // } else {
-    //   let destination = markers[0].props.coordinate;
-    // }
-      const destination = markers[0].props.coordinate ;
 
       return (
         <View>
@@ -98,32 +87,18 @@ class Map extends Component {
             onPress={(e) => this.onMapPress(e)}
             onRegionChange={this.onRegionChange.bind(this)}
           >
-            <MapViewDirections
-              origin={origin}
-              destination={destination || null}
-              apikey={}
-              strokeWidth={3}
-              strokeColor="hotpink"
-              onStart={(params) => {
-                console.log(`Started routing between "${params.origin}" and "${params.destination}"`);
-              }}
-              onReady={result => {
-                console.log(`Distance: ${result.distance} km`)
-                console.log(`Duration: ${result.duration} min.`)
-  
-                this.mapView.fitToCoordinates(result.coordinates, {
-                  edgePadding: {
-                    right: (width / 20),
-                    bottom: (height / 20),
-                    left: (width / 20),
-                    top: (height / 20),
-                  }
-                });
-              }}
-              onError={(errorMessage) => {
-                console.log('GOT AN ERROR');
-              }}
-            />
+            {this.props.wayPoints[0].length >= 2 ?
+            <>
+              {console.log('origin', this.props.wayPoints[0])}
+              {console.log('destination', this.props.wayPoints[0][1])}
+             <MapView.Polyline
+              coordinates={this.props.wayPoints[0]}
+              strokeWidth={5}
+              strokeColor={'#0CA5E7'}
+           />
+           </>
+            : null  
+            }
             {markers}
           </MapView>
         </View>

e9031ae4cecbf596dd66ccdf7ed0bfbfca3d88df
Megan Huggins <Muggins1265@gmail.com>
2020-07-27 22:47:09 -0600

    Start set up between BE and FE

--- src/User/Api/actions.js
+++ src/User/Api/actions.js
@@ -1,3 +1,7 @@
+// Imports
+import axios from 'axios';
+import { mutation } from "gql-query-builder";
+
 // Action Exports
 export const SET_USER_INFO = 'SET_USER_INFO';
 export const SET_ROUTE_INFO = 'SET_ROUTE_INFO';
@@ -17,4 +21,54 @@ export const setRouteInfo = (waypoints) => {
         type: 'SET_ROUTE_INFO',
         waypoints
     }
+}
+
+export function postUserForm(userForm) {  
+    // const body = {
+    //     query: `
+    //       mutation ($userEmail: String!) {
+    //         checkIn (
+    //           userEmail: $userEmail
+    //         ) {
+    //           user{
+    //           name
+    //         }
+    //         }
+    //       }
+    //     `,
+    //     variables: {
+    //         userEmail: "taco@taco.com"
+    //     }
+    // }
+    const body = {
+        query: `
+          mutation ($user: UserInput!, $contact: ContactInput!, $route: RouteInput!) {
+            createHifu(
+            user: $user,
+            contact: $contact,
+            route: $route
+        ){
+          user{
+            name
+          }
+        }
+    }`,
+        variables: {
+          user: userForm.user,
+          contact: userForm.contact,
+          route: userForm.route
+        }
+      }
+    console.log('hittin dat API', body);   
+    return dispatch => {
+      return axios.post('http://192.168.0.8/api/v1', 
+      body)
+      .then((response) => {
+        if (response.status === 200) {
+            console.log('response', response);
+        } else {
+            console.log("error");
+        }
+	});
+  }
 }
